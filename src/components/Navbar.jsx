a6ef9d921055226c9d5a88bbcd5d926bb07d0093
susmita debnath <susmita.debnath.cse@gmail.com>
2023-10-22 22:14:17 +0600

    add meta tag

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import MetaTags from 'react-meta-tags';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 
 function Navbar() {
@@ -8,7 +9,17 @@ function Navbar() {
     }
 
     return (
+
         <div className='flex justify-between items-center h-24 text-white max-w-[1240px] mx-auto px-4'>
+            <div className="wrapper">
+                <MetaTags>
+                    <title>Home</title>
+                    <meta name="description" content="Some description." />
+                    <meta property="og:title" content="MyApp" />
+                    <meta property="og:image" content="assets/data-img.png" />
+                </MetaTags>
+                {/* <div className="content"> Some Content </div> */}
+            </div>
             <h1 className='w-full text-3xl text-[#c6fbfc] font-bold'>Demo</h1>
             <ul className='hidden md:flex'>
                 <li className='p-4'>Home</li>
@@ -20,7 +31,7 @@ function Navbar() {
             <div onClick={handleNav} className='block md:hidden'>
                 {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
             </div>
-            <div className={nav?'fixed left-0 top-0 w-[60%] border-r h-full bg-[#000300] border-gray-900 ease-in-out duration-500':'fixed left-[-100%]'}>
+            <div className={nav ? 'fixed left-0 top-0 w-[60%] border-r h-full bg-[#000300] border-gray-900 ease-in-out duration-500' : 'fixed left-[-100%]'}>
                 <h1 className='w-full text-3xl text-[#c6fbfc] font-bold m-4'>Demo</h1>
                 <ul className='uppercase p-4'>
                     <li className='p-4 border-b border-gray-500'>Home</li>
